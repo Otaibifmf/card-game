e22b25b853be6194a541077c613bc26f86a15189
sackboy <118112953+DrSackboy@users.noreply.github.com>
2025-07-11 16:33:29 +0300

    Added a shitty card slot, cards work tho

--- CardManager.gd
+++ CardManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 var card_being_dragged
 var drag_offset = Vector2.ZERO
@@ -46,7 +47,14 @@ func start_drag(card):
 
 
 func finish_drag():
+	print("Running")
 	card_being_dragged.scale = Vector2(1.05, 1.05)
+	var card_slot_found = raycast_check_for_card_slot()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		print("Card slot found")
+		card_being_dragged.position = card_slot_found.position
+		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
 	card_being_dragged = null
 
 func connect_card_signals(card):
@@ -79,7 +87,18 @@ func highlight_card(card, hovered):
 		card.scale = Vector2(1, 1)
 		card.z_index = 1 
 
-
+func raycast_check_for_card_slot():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
+	
+	
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
@@ -88,7 +107,6 @@ func raycast_check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		#return result[0].collider.get_parent()
 		return get_card_with_highest_z_index(result)
 	return null
 	
